65b091b717ce7285bf2e584bbbb9202f7093fb0d
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-07-14 23:20:16 +0700

    feat:weather app

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Andreas Nugroho on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
